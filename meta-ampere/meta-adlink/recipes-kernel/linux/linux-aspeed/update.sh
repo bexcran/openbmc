ac283dd4d7195d8d6b61b3935d3da3436a29a02c
Rebecca Cran <rebecca@bsdio.com>
2023-12-28 21:58:09 -0700

    Delete update.sh script that was mistakenly added
    Signed-off-by: Rebecca Cran <rebecca@bsdio.com>

--- meta-ampere/meta-adlink/recipes-kernel/linux/linux-aspeed/update.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#set -e
-
-cp -fv aspeed-bmc-adlink-comhpcalt.dts /home/bcran/openbmc/build/comhpcalt/workspace/sources/linux-aspeed/oe-local-files/aspeed-bmc-adlink-comhpcalt.dts
-cp -fv aspeed-bmc-adlink-comhpcalt.dts /home/bcran/openbmc/build/comhpcalt/workspace/sources/linux-aspeed/arch/arm/boot/dts/aspeed-bmc-adlink-comhpcalt.dts
