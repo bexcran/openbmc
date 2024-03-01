00dbebc9089d6a6d2a6e5ba6fb820ebe1f1b0afd
Rebecca Cran <rebecca@bsdio.com>
2024-03-01 09:20:46 -0700

    Update adlink_update_mac.sh to set eth1addr in u-boot env
    It's now expected that both ethaddr and eth1addr are set in the u-boot
    environment. So, update adlink_update_mac.sh to set both the internal
    and external network interface MAC addresses.
    
    Signed-off-by: Rebecca Cran <rebecca@bsdio.com>

--- meta-ampere/meta-adlink/recipes-ampere/platform/ampere-utils/adlink_update_mac.sh
+++ meta-ampere/meta-adlink/recipes-ampere/platform/ampere-utils/adlink_update_mac.sh
@@ -5,60 +5,50 @@
 # environment, that's used in preference to the hard-coded address.
 #
 
-function Usage () {
-	echo "Usage:"
-	echo "      adlink_update_mac.sh <ethX>"
-	echo "Example:"
-	echo "      adlink_update_mac.sh eth0"
-}
-
-ETHERNET_INTERFACE=$1
-
-# Check eth port
-case ${ETHERNET_INTERFACE} in
-	"eth0")
-		ENV_PORT="1"
-		;;
-	"eth1")
-		ENV_PORT="2"
-		;;
-	"eth2")
-		ENV_PORT="3"
-		;;
-	*)
-		Usage
-		exit
-		;;
-esac
-
-MAC_ADDR=00:30:64:33:87:e5
+ETH0_MAC_ADDR=00:30:64:33:87:e5
+ETH1_MAC_ADDR=06:c2:49:a6:09:3b
 
 if fw_printenv "ethaddr" 2>/dev/null; then
-  MAC_ADDR=$(fw_printenv "ethaddr" 2>/dev/null | sed "s/^ethaddr=//")
+  ETH0_MAC_ADDR=$(fw_printenv "ethaddr" 2>/dev/null | sed "s/^ethaddr=//")
+fi
+
+if fw_printenv "eth1addr" 2>/dev/null; then
+  ETH1_MAC_ADDR=$(fw_printenv "eth1addr" 2>/dev/null | sed "s/^eth1addr=//")
 fi
 
 # Check if the Ethernet port has correct MAC Address
-ETH_INCLUDE_MAC=$(ip link show "${ETHERNET_INTERFACE}" | grep link | awk '{print $2}' | grep -i "$MAC_ADDR")
-if [ -n "$ETH_INCLUDE_MAC" ]; then
-	echo "BMC MAC Address is already configured"
-	exit 0
+ETH0_INCLUDE_MAC=$(ip link show eth0 | grep link | awk '{print $2}' | grep -i "$ETH0_MAC_ADDR")
+if [ -n "$ETH0_INCLUDE_MAC" ]; then
+	echo "MAC Addresses are already configured"
+        exit 0
 fi
 
-# Request to restart the service
-ip link set "${ETHERNET_INTERFACE}" down
-fw_setenv ethaddr "${MAC_ADDR}"
+for i in 0 1; do
+        INTERFACE=eth${i}
+        MAC_ADDR=ETH${i}_MAC_ADDR
+        ENV_PORT=$((i+1))
+        if [ $i = 0 ]; then
+                UBOOTENV=ethaddr
+        else
+                UBOOTENV=eth1addr
+        fi
+        # Request to restart the service
+        ip link set "${INTERFACE}" down
+        fw_setenv ${UBOOTENV} "${!MAC_ADDR}"
 
-ip link set dev "${ETHERNET_INTERFACE}" address "${MAC_ADDR}"
-retval=$?
-if [[ $retval -ne 0 ]]; then
-	echo "ERROR: Can not update ${ETHERNET_INTERFACE} MAC ADDR"
-	exit 1
-fi
-# Setting LAN MAC Address to xx:xx:xx:xx:xx:xx
-ipmitool lan set "${ENV_PORT}" macaddr "${MAC_ADDR}"
-# Enabling BMC-generated ARP responses & setting SNMP Community String to public
-ipmitool lan set "${ENV_PORT}" arp respond on
-ipmitool lan set "${ENV_PORT}" snmp public
-ip link set "${ETHERNET_INTERFACE}" up
-
-echo "Successfully updated the MAC address ${MAC_ADDR} for ${ETHERNET_INTERFACE}"
+        ip link set dev "${INTERFACE}" address "${!MAC_ADDR}"
+        retval=$?
+        if [[ $retval -ne 0 ]]; then
+	        echo "ERROR: Can not update ${INTERFACE} MAC ADDR"
+	        exit 1
+        fi
+        # Setting LAN MAC Address to xx:xx:xx:xx:xx:xx
+        ipmitool lan set "${ENV_PORT}" macaddr "${!MAC_ADDR}"
+        # Enabling BMC-generated ARP responses & setting SNMP Community String to public
+        if [ $i = 0 ]; then
+                ipmitool lan set "${ENV_PORT}" arp respond on
+                ipmitool lan set "${ENV_PORT}" snmp public
+                ip link set "${INTERFACE}" up
+        fi
+        echo "Successfully updated the MAC address ${!MAC_ADDR} for ${INTERFACE}"
+done
