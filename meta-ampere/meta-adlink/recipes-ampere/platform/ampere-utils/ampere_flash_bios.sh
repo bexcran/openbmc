d2ec3e5fc23d99675937cc8b1b9a18afb53480e6
Rebecca Cran <rebecca@bsdio.com>
2024-10-12 20:28:41 -0600

    meta-ampere/meta-adlink: Update ampere_flash_bios.sh
    Update ampere_flash_bios.sh to support flashing the whole SPI-NOR
    device, the TF-A+UEFI areas, and just the TF-A or UEFI sections.
    
    Since flashcp.mtd-utils can be given an MTD label, use that instead of
    searching for device names in /proc/mtd.
    
    Signed-off-by: Rebecca Cran <rebecca@bsdio.com>

--- meta-ampere/meta-adlink/recipes-ampere/platform/ampere-utils/ampere_flash_bios.sh
+++ meta-ampere/meta-adlink/recipes-ampere/platform/ampere-utils/ampere_flash_bios.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#
+
 # Copyright (c) 2021 Ampere Computing LLC
-#
+
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -13,110 +13,184 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 # shellcheck disable=SC2046
 
-do_flash () {
-	# Check the PNOR partition available
-	HOST_FW_MTD=$(< /proc/mtd grep "\"pnor-uefi\"" | sed -n 's/^\(.*\):.*/\1/p')
-	HOST_FULL_MTD=$(< /proc/mtd grep "\"pnor\"" | sed -n 's/^\(.*\):.*/\1/p')
-	if [ -z "$HOST_FW_MTD" ];
-	then
+set -o errexit
+set -o nounset
+
+do_probe () {
+	# Check the PNOR partition is available
+	if ! grep "pnor" /proc/mtd > /dev/null; then
 		# Check the ASpeed SMC driver bound before
 		HOST_SPI=/sys/bus/platform/drivers/spi-aspeed-smc/1e630000.spi
 		if [ -d "$HOST_SPI" ]; then
-			echo "Unbind the ASpeed SMC driver"
+			echo "Unbinding the ASpeed SMC driver"
 			echo 1e630000.spi > /sys/bus/platform/drivers/spi-aspeed-smc/unbind
 			sleep 2
 		fi
 
 		# If the PNOR partition is not available, then bind again driver
-		echo "--- Bind the ASpeed SMC driver"
+		echo "Binding the ASpeed SMC driver"
 		echo 1e630000.spi > /sys/bus/platform/drivers/spi-aspeed-smc/bind
 		sleep 2
-
-		HOST_FW_MTD=$(< /proc/mtd grep "\"pnor-uefi\"" | sed -n 's/^\(.*\):.*/\1/p')
-		HOST_FULL_MTD=$(< /proc/mtd grep "\"pnor\"" | sed -n 's/^\(.*\):.*/\1/p')
-		if [ -z "$HOST_FW_MTD" ];
-		then
-			echo "Fail to probe Host SPI-NOR device"
-			exit 1
-		fi
 	fi
 
-	extension=${IMAGE##*.}
-
-	if [ "$extension" = "img" ]; then
-		echo "--- Flashing firmware to @/dev/$HOST_FW_MTD"
-		flashcp.mtd-utils -p -v "$IMAGE" /dev/"$HOST_FW_MTD"
-	elif [ "$extension" = "bin" ]; then
-                echo "--- Flashing firmware to @/dev/$HOST_FULL_MTD"
-                flashcp.mtd-utils -p -v "$IMAGE" /dev/"$HOST_FULL_MTD"
-	else
-		echo "ERROR: Unknown file extension \"$extension\""
+	if ! grep "pnor" /proc/mtd > /dev/null; then
+		echo "Error: failed to probe host SPI-NOR device"
+		exit 1
 	fi
 }
 
+turn_host_off () {
+	# Turn off the Host if it is currently ON
+	chassisstate=$(ipmitool power status | awk '{print $4}')
+	echo "Current chassis state: $chassisstate"
+	if [ "$chassisstate" == 'on' ]; then
+		echo "Turning the chassis off"
+		ipmitool power off > /dev/null
 
-if [ $# -eq 0 ]; then
-	echo "Usage: $(basename "$0") <UEFI image file>"
-	exit 0
-fi
+		# Wait 60s until Chassis is off
+		cnt=30
+		while [ "$cnt" -gt 0 ]; do
+			cnt=$((cnt - 1))
+			sleep 2
+			# Check if HOST was OFF
+			chassisstate_off=$(ipmitool power status | awk '{print $4}')
+			if [ "$chassisstate_off" != 'on' ]; then
+				break
+			fi
 
-IMAGE="$1"
-if [ ! -f "$IMAGE" ]; then
-	echo "The image file $IMAGE does not exist"
-	exit 1
-fi
+			if [ "$cnt" == "0" ]; then
+				echo "Error: failed to turn the chassis off"
+				exit 1
+			fi
+		done
+	fi
+}
 
-# Turn off the Host if it is currently ON
-chassisstate=$(ipmitool power status | awk '{print $4}')
-echo "--- Current Chassis State: $chassisstate"
-if [ "$chassisstate" == 'on' ];
-then
-	echo "--- Turning the Chassis off"
-	ipmitool power off
-
-	# Wait 60s until Chassis is off
-	cnt=30
-	while [ "$cnt" -gt 0 ];
-	do
-		cnt=$((cnt - 1))
-		sleep 2
-		# Check if HOST was OFF
-		chassisstate_off=$(ipmitool power status | awk '{print $4}')
-		if [ "$chassisstate_off" != 'on' ];
-		then
-			break
-		fi
-
-		if [ "$cnt" == "0" ];
-		then
-			echo "--- Error : Failed turning the Chassis off"
+switch_spi_bus () {
+	if [ "$1" = "bmc" ]; then
+		# Switch the host SPI bus to BMC"
+		echo "Switching the host SPI bus to BMC"
+		if ! gpioset $(gpiofind spi0-program-sel)=0; then
+			echo "Error: failed to switch the host SPI bus to BMC. Please check GPIO state"
 			exit 1
 		fi
-	done
+	else
+		# Switch the host SPI bus to HOST."
+		echo "Switching the host SPI bus to Host"
+		if ! gpioset $(gpiofind spi0-program-sel)=1; then
+			echo "Error: failed to switch the host SPI bus to Host. Please check GPIO state"
+			exit 1
+		fi
+	fi
+}
+
+do_flash() {
+	case "$IMAGE_TYPE" in
+		full)
+			MTD=mtd:pnor
+			;;
+		code)
+			MTD=mtd:pnor-code
+			;;
+		tfa)
+			MTD=mtd:pnor-tfa
+			;;
+		uefi)
+			MTD=mtd:pnor-uefi
+			;;
+	esac
+
+	echo "Flashing firmware to ${MTD}"
+	flashcp.mtd-utils -p -v "$IMAGE" "${MTD}"
+}
+
+usage () {
+	echo "Utility to flash the host (arm64/AArch64/ARMv8-A CPU) firmware on Ampere systems."
+	echo "Usage:"
+	echo "  $(basename "$0") [options] <Host firmware file>"
+	echo
+	echo "Options:"
+	echo "  -f, --full                       Flash the entire SPI-NOR chip (default)"
+	echo "  -c, --code                       Flash the code (TF-A + UEFI) area"
+	echo "  -t, --tfa                        Flash the TF-A (ATF) area"
+	echo "  -u, --uefi                       Flash the UEFI area"
+	echo "  -B, --bmc                        Switch the Host SPI bus to the BMC and exit"
+	echo "  -H, --host                       Switch the Host SPI bus to the Host and exit"
+	echo "  -h, --help                       This help message"
+	echo ""
+	echo "Note: TF-A (Trusted Firmware for ARMv8-A) is the same as ATF (ARM Trusted Firmware)."
+	exit 1
+}
+
+# Script starts running here
+
+IMAGE_TYPE=""
+
+OPTIONS=$(getopt -o fctuBHh --long full,code,tfa,uefi,bmc,host,help -- "$@")
+eval set -- "$OPTIONS"
+
+while true; do
+	case "$1" in
+		-f|--full)
+			IMAGE_TYPE=full; shift;;
+		-c|--code)
+			IMAGE_TYPE=code; shift;;
+		-t|--tfa)
+			IMAGE_TYPE=tfa; shift;;
+		-u|--uefi)
+			IMAGE_TYPE=uefi; shift;;
+		-B|--bmc)
+			switch_spi_bus bmc;
+			do_probe;
+			exit;;
+		-H|--host)
+			switch_spi_bus host;
+			exit;;
+		-h|--help)
+			usage;;
+		--) shift; break;;
+		*) echo "Internal error ($1)!"; exit 1;;
+	esac
+done
+
+if [ "$#" -eq 1 ] && [ -n "$1" ]; then
+	IMAGE="$1"
+else
+	usage
 fi
 
-# Switch the host SPI bus to BMC"
-echo "--- Switch the host SPI bus to BMC."
-if ! gpioset $(gpiofind spi0-program-sel)=0; then
-	echo "ERROR: Switch the host SPI bus to BMC. Please check gpio state"
+eval set -- ""
+
+if [ -z "${IMAGE_TYPE}" ]; then
+	extension=${IMAGE##*.}
+	if [ "$extension" = "img" ];  then
+		IMAGE_TYPE=code
+	elif [ "$extension" = "bin" ]; then
+		IMAGE_TYPE=full
+	fi
+fi
+
+if [ ! -f "${IMAGE}" ]; then
+	echo "The image file ${IMAGE} does not exist"
 	exit 1
 fi
+
+turn_host_off
+
+switch_spi_bus bmc
+
+do_probe
 
 # Flash the firmware
 do_flash
 
-# Switch the host SPI bus to HOST."
-echo "--- Switch the host SPI bus to HOST."
-if ! gpioset $(gpiofind spi0-program-sel)=1; then
-	echo "ERROR: Switch the host SPI bus to HOST. Please check gpio state"
-	exit 1
-fi
+switch_spi_bus host
 
-if [ "$chassisstate" == 'on' ];
-then
-	sleep 5
-	echo "Turn on the Host"
-	ipmitool power on
+if [ "$chassisstate" == 'on' ]; then
+	sleep 2
+	echo "Turning on the host"
+	ipmitool power on > /dev/null
 fi
